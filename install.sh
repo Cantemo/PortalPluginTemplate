b40c1eebf89cdf8ad3ecaf14d6d311a95e90e601
Parham Azimi <parham@cantemo.com>
2011-08-26 08:05:11 +0000

    Added copyright, and a readme file for installation. Also updated the install script to install to the core theme.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 PORTAL_ROOT="/opt/cantemo/portal"
 PLUGIN_NAME="PortalPluginTemplate"
 sudo mkdir -p $PORTAL_ROOT/portal/plugins/$PLUGIN_NAME
-sudo mkdir -p $PORTAL_ROOT/portal_themes/sand/templates/plugins/$PLUGIN_NAME
+sudo mkdir -p $PORTAL_ROOT/portal_themes/core/templates/plugins/$PLUGIN_NAME
 
 sudo cp *.py $PORTAL_ROOT/portal/plugins/$PLUGIN_NAME
-sudo cp templates/* $PORTAL_ROOT/portal_themes/sand/templates/plugins/$PLUGIN_NAME
+sudo cp templates/* $PORTAL_ROOT/portal_themes/core/templates/plugins/$PLUGIN_NAME
