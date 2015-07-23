a5b4ee62d26b8951ca34e04adf56fc6a2cd4d152
Mattias Amnefelt <mattiasa@cantemo.com>
2015-07-23 17:16:59 +0200

    Import of dashboard widget only works on Portal 2.1 or later

--- __init__.py
+++ __init__.py
@@ -2,4 +2,8 @@
 Import all plugins here
 '''
 from plugin import *
-from dashboard_widget_examples import *
+try:
+    from dashboard_widget_examples import *
+except ImportError:
+    # This only works on portal 2.1 or later
+    pass
